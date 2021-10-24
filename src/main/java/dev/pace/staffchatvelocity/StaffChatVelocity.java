b93a3be14bec4d77de8f45fe2c65284952c8f602
Pace <pacex3123@gmail.com>
2021-10-24 13:07:29 +0200

    Initial commit

---
+++ src/main/java/dev/pace/staffchatvelocity/StaffChatVelocity.java
@@ -0,0 +1,58 @@
+package dev.pace.staffchatvelocity;
+
+import com.google.inject.Inject;
+import com.mojang.brigadier.arguments.StringArgumentType;
+import com.mojang.brigadier.builder.LiteralArgumentBuilder;
+import com.mojang.brigadier.builder.RequiredArgumentBuilder;
+import com.mojang.brigadier.tree.LiteralCommandNode;
+import com.velocitypowered.api.command.BrigadierCommand;
+import com.velocitypowered.api.command.CommandManager;
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.event.proxy.ProxyInitializeEvent;
+import com.velocitypowered.api.event.Subscribe;
+import com.velocitypowered.api.plugin.Plugin;
+import com.velocitypowered.api.proxy.ProxyServer;
+import dev.pace.staffchatvelocity.cmd.StaffChat;
+import org.slf4j.Logger;
+
+/**
+ * Created by Pace
+ * No part of this publication may be reproduced, disturbed, or transmitted in any form or any means.
+ */
+
+@Plugin(
+        id = "staffchat",
+        name = "StaffChatVelocity",
+        version = "1.0",
+        description = "StaffChat for Velocity Minecraft proxies.",
+        url = "https://pace.is-a.dev/",
+        authors = {"Pace"}
+)
+public class StaffChatVelocity {
+
+    @Inject
+    private Logger logger;
+
+    @Inject
+    private ProxyServer proxyServer;
+
+    @Inject
+    private StaffChat broadcastCommand;
+
+    @Subscribe
+    public void onProxyInitialization(ProxyInitializeEvent event) {
+        LiteralCommandNode<CommandSource> broadcast = LiteralArgumentBuilder.<CommandSource>literal("staffchat")
+                .requires(ctx -> ctx.hasPermission("staffchat.use"))
+                .then(RequiredArgumentBuilder.<CommandSource, String>argument("message", StringArgumentType.greedyString())
+                        .executes(broadcastCommand::execute)
+                        .build())
+                .build();
+
+
+        CommandManager manager = proxyServer.getCommandManager();
+        manager.register(
+                manager.metaBuilder("staffchat").aliases("sc").build(),
+                new BrigadierCommand(broadcast)
+        );
+    }
+}
---
+++ src/main/java/dev/pace/staffchatvelocity/cmd/StaffChat.java
@@ -0,0 +1,91 @@
+package dev.pace.staffchatvelocity.cmd;
+
+import com.google.inject.Inject;
+import com.mojang.brigadier.context.CommandContext;
+import com.velocitypowered.api.command.CommandSource;
+import com.velocitypowered.api.proxy.Player;
+import com.velocitypowered.api.proxy.ProxyServer;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.format.NamedTextColor;
+import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+
+/**
+ * Created by Pace
+ * No part of this publication may be reproduced, disturbed, or transmitted in any form or any means.
+ */
+
+public class StaffChat {
+
+    @Inject
+    private ProxyServer proxyServer;
+
+    public String getName(CommandSource source) {
+        if (source instanceof Player)
+            return ((Player) source).getUsername();
+        return "Console";
+    }
+
+    public String getServerName(CommandSource source) {
+        if (source instanceof Player)
+            return ((Player) source).getCurrentServer().get().getServerInfo().getName();
+        return "proxy";
+    }
+
+    public int execute(CommandContext<CommandSource> ctx) {
+        if (ctx.getSource().hasPermission("staffchat.velocity")) {
+            Component deserialized = Component.text()
+                    .append(Component.text()
+                            .color(NamedTextColor.DARK_GRAY)
+                            .append(Component.text("["))
+                            .build()
+                    )
+                    .append(Component.text()
+                            .color(NamedTextColor.YELLOW)
+                            .append(Component.text("StaffChat"))
+                            .build()
+                    )
+                    .append(Component.text()
+                            .color(NamedTextColor.DARK_GRAY)
+                            .append(Component.text("] "))
+                            .build()
+                    )
+                    .append(Component.text()
+                            .color(NamedTextColor.DARK_GRAY)
+                            .append(Component.text("["))
+                            .build()
+                    )
+                    .append(Component.text()
+                            .color(NamedTextColor.YELLOW)
+                            .append(Component.text(getServerName(ctx.getSource())))
+                            .build()
+                    )
+                    .append(Component.text()
+                            .color(NamedTextColor.DARK_GRAY)
+                            .append(Component.text("] "))
+                            .build()
+                    )
+                    .append(Component.text()
+                            .color(NamedTextColor.WHITE)
+                            .append(Component.text(getName(ctx.getSource())))
+                            .build()
+                    )
+                    .append(Component.text()
+                            .color(NamedTextColor.WHITE)
+                            .append(Component.text(": "))
+                            .build()
+                    )
+                    .append(LegacyComponentSerializer.legacyAmpersand().deserialize("&f" + ctx.getArgument("message", String.class)))
+                    .build();
+            proxyServer.getAllPlayers().stream().filter(target -> target.hasPermission("staffchat.use")).forEach(player -> player.sendMessage(deserialized));
+        } else {
+            Component deserialized = Component.text()
+                    .append(Component.text()
+                            .color(NamedTextColor.RED)
+                            .append(Component.text("You do not have permission."))
+                            .build()
+                    )
+                    .build();
+        }
+        return 1;
+    }
+}
